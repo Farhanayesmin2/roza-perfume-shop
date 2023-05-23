b1e04943e3c88a808070da358d773b1dcda6d0d5
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-23 18:32:57 +0600

    Layout fixed

--- src/Component/Router/router.jsx
+++ src/Component/Router/router.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
 import Home from "../Page/Home/Home";
-import Main from "../Main/Layout"
+
 import NotFound from "../Page/NotFound/NotFound";
 const router = createBrowserRouter([
   {
