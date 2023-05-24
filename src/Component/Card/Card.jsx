dc4b4bf6ccac4cfd7fac190ae90dfd2eac434d51
MNSM92 <mnsm.pbl@gmail.com>
2023-05-24 13:56:31 -0400

    new

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import PopupForm from "../pages/PopUp";
+import { useState } from "react"; 
 
-const SingleProduct = ({ product }) => {
-  const { img, title, brand, price } = product;
+const Card = () => {
+  
   const [TR, setTR] = useState('false');
 
 
@@ -11,31 +10,30 @@ const SingleProduct = ({ product }) => {
     <>
 
   
-    <div className="single-product flex flex-col bg-gray-50 gap-3 shadow-md hover:shadow-xl hover:scale-105 duration-300 px-4 py-7 rounded-sm overflow-hidden">
+    <div className="flex flex-col items-center bg-gray-50 gap-3 shadow-md hover:shadow-xl hover:scale-105 duration-300 px-4 py-7 rounded-sm overflow-hidden">
       <div className="flex justify-center">
         <img
-          className="w-72 h-48 object-contain hover:scale-110 duration-500"
-          src={img}
-          alt={title}
+          className="w-48 h-48 object-contain hover:scale-110 duration-500"
+          src="https://freesvg.org/img/Wine-bottle.png"
+          alt="safd"
         />
       </div>
       <Link
-        to={title}
-        state={product}
-        className="hover:text-rose-500 duration-300 flex justify-between items-center"
+        to=""
+        state="" className="hover:text-rose-500 duration-300 flex justify-between items-center"
       >
         <h2 className="text-stone-950 font-semibold text-xl capitalize">
-          {product.title.slice(0, 20)}
+          HGJ
         </h2>
       </Link>
       <p className="text-sm text-gray-600">
-        Location: <span className="font-semibold capitalize">{brand}</span>
+        Location: <span className="font-semibold capitalize">sf</span>
       </p>
       <p className="text-sm text-gray-600">
-      Rresale Price: <span className="text-rose-500 font-semibold">{price}</span>
+      Rresale Price: <span className="text-rose-500 font-semibold">asf</span>
       </p>
       <p className="text-sm text-gray-600">
-      Original Price: <span className="text-rose-500 font-semibold">{price}</span>
+      Original Price: <span className="text-rose-500 font-semibold">saf</span>
       </p>
       <p className="text-sm text-gray-600">
       years of use: <span className="text-rose-500 font-semibold">3 Years</span>
@@ -45,20 +43,15 @@ const SingleProduct = ({ product }) => {
       </p>
       <div className="flex justify-between items-center">
         <Link
-          to={title}
-          state={product}
+          to=""
+          state=""
           className="hover:text-rose-50 text-gray-900 duration-300 flex justify-between items-center"
         >
           <button className="text-sky-400 px-2 py-1 border border-sky-400 rounded-md hover:bg-sky-400 hover:text-sky-50 duration-300">
-            More Info
+            Add to Bag
           </button>
         </Link>
-        <div
-          onClick={() => setTR('true')}
-          className="bg-sky-400 text-sky-50 hover:bg-sky-50 hover:text-sky-400 duration-300 border border-sky-400 px-2 py-1 rounded-md"
-        >
-          <PopupForm IO={TR} name={title} price={price}/>
-        </div>
+        
         
       </div>
     </div>
--- src/Component/Page/Home/Home.jsx
+++ src/Component/Page/Home/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Slider from '../../Slider/Slider';
 import Banner2 from "../../Shared/Navbar/Banner/Banner"
 import Banner from '../../Banner/Banner';
+import Card from '../../Card/Card';
 
 const Home = () => {
     return (
@@ -81,17 +82,11 @@ const Home = () => {
              </div>
              
 
-             <div className='grid grid-cols-3 justify-between relative h-auto w-[80%] left-[10%] gap-3 mt-10 mb-10'>
+             <div className='grid grid-cols-4 justify-between relative h-auto w-[80%] left-[10%] gap-3 mt-10 mb-10'>
                
-                    <div className='relative col-span-1 h-[316px] left-0 top-0'>
-                    <img
-                        className="relative w-full h-full left-0 top-0"
-                       
-                        src="https://freesvg.org/img/Wine-bottle.png"
-                    />
-                        <p className='absolute left-20 top-40 text-gray-500 font-bold'>WOMANS PERFUME</p>
-
-                    </div> 
+                    <Card/>
+                    <Card/>
+                    <Card/>
                     </div> 
 
     
