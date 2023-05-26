0db1f0629fd65ecdc6add6f0fbebb0e6a9bfda82
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-26 13:14:19 +0600

    New arrivals added

--- src/Component/Page/Home/Home.jsx
+++ src/Component/Page/Home/Home.jsx
@@ -2,29 +2,18 @@ import React from 'react';
 
 import Banner from '../../Shared/Navbar/Banner/Banner';
 import ShopCollection from '../../Section/ShopCollection/ShopCollection';
-<<<<<<< HEAD
 import BestSellers from '../../Section/ShopCollection/BestSellers/BestSellers';
-
-=======
-import ItemsHome from '../../ShopItems/ItemsHome'; 
->>>>>>> b3ac25a2e547ff1165699d20a204e10f780d2ae8
+import NewProducts from '../../Section/NewProducts/NewProducts';
 
 const Home = () => {
     return (
         <div >
        
-<<<<<<< HEAD
            <Banner></Banner>
-<ShopCollection></ShopCollection>
+            <ShopCollection></ShopCollection>
             <BestSellers></BestSellers>
-         
-=======
-            <Banner/>
-            <ShopCollection/>
-            <ItemsHome/>
-            
+            <NewProducts></NewProducts>
     
->>>>>>> b3ac25a2e547ff1165699d20a204e10f780d2ae8
         </div>
     );
 };
